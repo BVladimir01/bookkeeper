1f2636a89df5f9990a45b6f987a810fdabdf1103
Vladimir <bizin.vk19@physics.msu.ru>
2024-03-13 16:12:57 +0300

    created category model, added tests for it

---
+++ bookkeeper/models/category.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from bookkeeper.repository.abstract_repository import AbstractRepository
+
+@dataclass
+class Category:
+    """
+    Категория расходов, хранит название в
+    атрибуте name и ссылку (id) на родителя в
+    атрибуте parent (у категорий верхнего
+    уровня None).
+    """
+    name: str
+    parent: int | None = None
+    pk: int = 0
+
+    def get_parent(self, repo: AbstractRepository['Category']) -> 'Category | None':
+        """return parent object from repository"""
+        if self.parent is None:
+            return None
+        return repo.get(self.parent)
---
+++ tests/test_models/test_category.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('D:\\физтех\\proga\\bookkeeper_project')
+
+from bookkeeper.models.category import Category
+from bookkeeper.repository.memory_repository import MemoryRepository
+import pytest
+
+
+@pytest.fixture
+def repo():
+    return MemoryRepository()
+
+
+def test_create_object():
+    c = Category('name2')
+    assert c.name == 'name2'
+    assert c.parent == None
+    assert c.pk == 0
+
+    c = Category('name2', parent=2, pk=1)
+    assert c.name == 'name2'
+    assert c.parent == 2
+    assert c.pk == 1
+
+
+def test_reassign():
+    c = Category('name2', parent=2, pk=1)
+    c.parent = 1
+    c.pk = 2
+    assert c.parent == 1
+    assert c.pk == 2
+
+
+def test_eq():
+    c1 = Category('name2', parent=2, pk=1)
+    c2 = Category('name2', parent=2, pk=1)
+    assert not c1 is c2
+    assert c1 == c2
+
+
+def test_get_parent(repo):
+    c1 = Category(name='parent')
+    pk = repo.add(c1)
+    c2 = Category(name='name', parent=pk)
+    repo.add(c2)
+    print(repo.get_all())
+    assert c2.get_parent(repo) == c1
