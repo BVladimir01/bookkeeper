fe9aef0b7d6af490d78555b1978f0c535d1adbf2
Vladimir <bizin.vk19@physics.msu.ru>
2024-03-13 16:59:04 +0300

    added expense model, tests for expense

---
+++ bookkeeper/models/expense.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from bookkeeper.repository.abstract_repository import AbstractRepository
+from datetime import datetime
+
+
+@dataclass
+class Expense:
+    """
+    Категория расходов, хранит название в
+    атрибуте name и ссылку (id) на родителя в
+    атрибуте parent (у категорий верхнего
+    уровня None).
+    """
+    amount: int
+    category: int
+    pk: int = 0
+    expense_date: datetime = datetime.now()
+    added_date:  datetime = datetime.now()
+    comment: str = ''
+    pk: int = 0
