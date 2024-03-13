74977e5de314ab15c1b1c8c8ec70ea56396eaf78
Vladimir <bizin.vk19@physics.msu.ru>
2024-03-13 15:20:32 +0300

    sqlite repo: now creates tables in init; added add test for sqlite repo

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -5,31 +5,55 @@ import sqlite3
 
 
 class SQLiteRepository(AbstractRepository[T]):
+
     def __init__(self, db_file: str, cls: type) -> None:
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
-    
+        types = ',\n'.join([f'{field}' for field, typ in self.fields.items()])
+
+        conn = sqlite3.connect(self.db_file)
+        cursor = conn.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON')
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS {self.table_name} (
+                       id INTEGER PRIMARY KEY,
+                       {types});""")
+        conn.commit()
+        conn.close()
+
+
     def add(self, obj: T) -> int:
-        names = ', '.join(self.fields.keys)
-        p = ', '.joint('?'*len(self.fields))
+
+        if getattr(obj, 'pk', None) != 0:
+            raise ValueError('trying to add {} with filled pk attribute'.format(obj))
+        
+        names = ', '.join(self.fields.keys())
+        p = ', '.join('?'*len(self.fields))
         values = [getattr(obj, x) for x in self.fields]
+
         conn = sqlite3.connect(self.db_file)
         cursor = conn.cursor()
         cursor.execute('PRAGMA foreign_keys = ON')
         cursor.execute(f'INSERT into {self.table_name} ({names}) VALUES ({p})', values)
         obj.pk = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
         return obj.pk
     
+
     def delete(self, pk: int) -> None:
         pass
+
 
     def get_all(self, where: Dict[str, Any] | None = None) -> List[T]:
         pass
 
+
     def get(self, pk: int) -> T | None:
         pass
+
 
     def update(self, obj: T) -> None:
         pass
--- tests/test_repository/test_memory_repository.py
+++ tests/test_repository/test_memory_repository.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('D:\\физтех\\proga\\bookkeeper_project')
 
 from bookkeeper.repository.memory_repository import MemoryRepository
-from bookkeeper.repository.sqlite_repository import SQLiteRepository
 import pytest
 
 
@@ -10,7 +9,6 @@ import pytest
 def custom_class():
     class Custom():
         pk = 0
-        attr_name = 'name'
     
     return Custom
 
@@ -20,7 +18,6 @@ def repo():
     return MemoryRepository()
 
 
-@pytest.mark.parametrize('repo', MemoryRepository(), SQLiteRepository())
 def test_crud(custom_class, repo):
     obj = custom_class()
     pk = repo.add(obj)
