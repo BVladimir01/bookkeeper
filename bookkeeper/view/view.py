4a2c0018723a3488d1b4d335b12adff621019c8a
Vladimir <bizin.vk19@physics.msu.ru>
2024-04-08 15:17:57 +0300

    fixed pylint warnings in view

--- bookkeeper/view/view.py
+++ bookkeeper/view/view.py
@@ -1,12 +1,20 @@
-import sys
-from PySide6 import QtWidgets
+"""
+Describes MyWindow class - view
+"""
+
+
 from datetime import date
-from view.budget_table import *
-from view.category_view import *
-from view.expense_table import *
+from PySide6 import QtWidgets
+from view.budget_table import BudgetTable
+from view.category_view import CategoryDialog, CategoryDialogEdit
+from view.expense_table import ExpenseTable
 
 
 class MyWindow(QtWidgets.QWidget):
+    """
+    MyWindow class realizes view
+    Main window of Bookkeeper
+    """
 
     def __init__(self):
         super().__init__()
@@ -19,7 +27,9 @@ class MyWindow(QtWidgets.QWidget):
         self.resize(600, 800)
 
 
-    def init_ui(self):
+    def init_ui(self) -> None:
+        """Initiates widgets and layout of window"""
+
         self.general_layout = QtWidgets.QVBoxLayout()
 
         self.general_layout.addWidget(QtWidgets.QLabel('Записи'))
@@ -33,7 +43,9 @@ class MyWindow(QtWidgets.QWidget):
         self.setLayout(self.general_layout)
 
 
-    def add_edit_layout(self):
+    def add_edit_layout(self) -> None:
+        """Initiates layout for adding expense entries"""
+
         edit_layout = QtWidgets.QGridLayout()
 
         edit_layout.addWidget(QtWidgets.QLabel('Дата расхода'), 0, 0)
@@ -72,7 +84,13 @@ class MyWindow(QtWidgets.QWidget):
         self.general_layout.addLayout(edit_layout)
 
 
-    def add_exp_enrty_slot(self):
+    def add_exp_enrty_slot(self) -> None:
+        """
+        Called when add button is clicked
+        Collects data
+        Delegates addition of expense entry
+        """
+
         if not hasattr(self, 'chosen_category_pk'):
             self.chosen_category_pk = 0
         amount = self.edit_amount_line.text()
@@ -89,56 +107,134 @@ class MyWindow(QtWidgets.QWidget):
         delattr(self, 'chosen_category_pk')
         self.chosen_category_label.setText('')
 
-    def choose_category_slot(self):
+
+    def choose_category_slot(self) -> None:
+        """
+        Called when choose category button is clicked
+        Opens dialog window with categories to choose
+        """
+
         self.dlg = CategoryDialog(self, self.update_categories_func)
         self.update_categories_func()
-        res = self.dlg.exec()
+        self.dlg.exec()
 
 
-    def edit_category_slot(self):
+    def edit_category_slot(self) -> None:
+        """
+        Called when edit categores button is clicked
+        Opens dialog window with categories to edit
+        """
+
         self.dlg = CategoryDialogEdit(self, self.update_categories_func, self.change_category_func,
                                       self.add_category_func, self.delete_category_func)
         self.update_categories_func()
-        res = self.dlg.exec()
+        self.dlg.exec()
 
 
-    def register_exp_add(self, handler):
+    def register_exp_add(self, handler) -> None:
+        """
+        Registers handler as function
+        that adds new expense entry
+        """
         self.add_exp_func = handler
 
-    def register_exp_delete(self, handler):
+
+    def register_exp_delete(self, handler) -> None:
+        """
+        Registers handler as function
+        that delete expense entry
+        """
         self.expenses_table.register_delete(handler)
 
-    def update_expenses(self, expenses, categories):
+
+    def update_expenses(self, expenses, categories) -> None:
+        """
+        Called when presenter initiates update
+        of expenses in users interface with categories arg
+        Calls same methd of ExpenseTable
+        """
         self.expenses_table.update_expenses(expenses, categories)
 
-    def register_exp_change(self, handler):
+
+    def register_exp_change(self, handler) -> None:
+        """
+        Registers handler as function
+        that adds new expense entry
+        """
         self.expenses_table.register_change(handler)
 
-    def register_exp_category_change(self, handler):
+
+    def register_exp_category_change(self, handler) -> None:
+        """
+        Registers handler as function
+        that changes category of existing expense entry
+        """
         self.expenses_table.register_exp_category_change(handler)
 
-    def display_expenses(self, expenses):
+
+    def display_expenses(self, expenses) -> None:
+        """
+        Called when presenter initiates count
+        of expenses
+        Calls same method of BudgtetTable
+        """
         self.budget_table.display_expenses(expenses)
 
+
     def update_budgets(self, budgets):
+        """
+        Called when presenter initiates update
+        of budget with budgets arg
+        Calls same method of BudgtetTable
+        """
         self.budget_table.update_budgets(budgets)
 
-    def register_budget_change(self, handler):
+
+    def register_budget_change(self, handler) -> None:
+        """
+        Registers handler as function
+        that changes budget entry
+        """
         self.budget_table.register_budget_change(handler)
 
-    def register_update_categories(self, handler):
+
+    def register_update_categories(self, handler) -> None:
+        """
+        Registers handler as function
+        that updates categories initiated from view
+        """
         self.update_categories_func = handler
 
-    def update_categories(self, categories):
+
+    def update_categories(self, categories) -> None:
+        """
+        Called whe presenter initiates
+        update of categories in user interface
+        """
         self.dlg.update_categories(categories)
 
-    def register_category_change(self, handler):
+
+    def register_category_change(self, handler) -> None:
+        """
+        Registers handler as function
+        that changes category
+        """
         self.change_category_func = handler
 
-    def register_category_add(self, handler):
+
+    def register_category_add(self, handler) -> None:
+        """
+        Registers handler as function
+        that adds new category
+        """
         self.add_category_func = handler
-    
-    def register_category_delete(self, handler):
+
+
+    def register_category_delete(self, handler) -> None:
+        """
+        Registers handler as function
+        that deletes category
+        """
         self.delete_category_func = handler
 
     # def register_children_getter(self, handler):
@@ -147,8 +243,8 @@ class MyWindow(QtWidgets.QWidget):
     # def register_parent_getter(self, handler):
     #     self.parent_getter = handler
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication()
-    window = MyWindow()
-    window.show()
-    sys.exit(app.exec())
+# if __name__ == '__main__':
+#     app = QtWidgets.QApplication()
+#     window = MyWindow()
+#     window.show()
+#     sys.exit(app.exec())
