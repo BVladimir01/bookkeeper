48a75a9d00c946124465c41b7d7d890fd1e82a1c
Vladimir <bizin.vk19@physics.msu.ru>
2024-04-08 12:27:01 +0300

    fixed pylint warnings in view budget_table

--- bookkeeper/view/budget_table.py
+++ bookkeeper/view/budget_table.py
@@ -1,8 +1,19 @@
-from PySide6 import QtWidgets, QtGui, QtCore
-from PySide6.QtCore import QEvent, QObject, Qt, QSize, Signal
+"""
+Describes Class BudgetTable for MyWindow class
+"""
+
+
+from typing import List
+from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
+from bookkeeper.models.budget import Budget
+from bookkeeper.models.expense import Expense
 
 
 class BudgetTable(QtWidgets.QTableWidget):
+    """
+    Widget that displays budget requirements and counts expenses
+    """
 
     def __init__(self, parent: QtWidgets.QWidget):
         super().__init__(parent)
@@ -17,17 +28,28 @@ class BudgetTable(QtWidgets.QTableWidget):
         hor_header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         hor_header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         self.setFixedHeight(116)
-        
+
         # self.hideColumn(1)
 
         self.itemChanged.connect(self.change_slot)
 
 
     def register_budget_change(self, handler):
+        """
+        Connects handler function to this class
+        Handler function is from presenter,
+        it handles back end of budget change
+        """
+
         self.change_budget = handler
 
 
     def change_slot(self, item: QtWidgets.QTableWidgetItem):
+        """
+        Called when user changes budget via user inteface.
+        Calls register_budget_change
+        """
+
         if self.initiated:
             if item.column() == 2:
                 attr_val_dict = {}
@@ -44,17 +66,26 @@ class BudgetTable(QtWidgets.QTableWidget):
                     input()
 
 
-    def update_budgets(self, budgets):
+    def update_budgets(self, budgets: List[Budget]):
+        """
+        Renews budget entries in user interface
+        Gets information for update from budgets argument
+        """
+
         self.itemChanged.disconnect(self.change_slot)
         for i in range(3):
             self.setItem(i, 1, QtWidgets.QTableWidgetItem(str(budgets[i].pk)))
             self.setItem(i, 2, QtWidgets.QTableWidgetItem(str(budgets[i].amount)))
-        
+
         self.initiated = True
         self.itemChanged.connect(self.change_slot)
 
 
-    def display_expenses(self, expenses):
+    def display_expenses(self, expenses: List[Expense]):
+        """
+        Renews counted expenses in user interface
+        Gets information for update from expenses argument
+        """
         self.itemChanged.disconnect(self.change_slot)
         for i, expense in enumerate(expenses):
             self.setItem(i, 0, QtWidgets.QTableWidgetItem(str(expense)))
